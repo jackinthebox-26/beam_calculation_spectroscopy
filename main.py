258cd29c65781803a35507115bdce461251bf588
James Maldaner <maldaner@ualberta.ca>
2026-04-30 10:08:31 -0600

    Update main.py to be empty

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def initiate_beam(wavelength_center, wavelength_fwhm, gdd_initial, w_0, z_from_w_0=None, jones_matrix=None):
-    # Return the gaussian beam class in the initial codition
-    if z_from_w_0 is None: z_from_w_0 = 0
-    if jones_matrix is None: jones_matrix = [1, 0]
-
-
-            
-
-    pass
-
-
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
