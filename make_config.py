09682380e3f5ed14205e3772cfe6859b1d47d1c9
James Maldaner <maldaner@ualberta.ca>
2026-05-11 10:55:21 -0600

    Updated some values

--- make_config.py
+++ make_config.py
@@ -26,7 +26,7 @@ def initiate_thorlabs():
                                'transmission': 0.95,  # 
                                'index': 1.458,  # 
                                'aperture': 5.4e-3,  # m
-                               'GVD': 36e-10 # s^2/m
+                               'GVD': 36e-27 # s^2/m
                                 }
     make_configfile(lens_thorlabs_LA4380_UV, folder)
     logger.info('Done thorlabs config.')
@@ -38,32 +38,33 @@ def initiate_data():
     coll_lens = {'name': 'collimating lens',
                  'filename': 'config_coll_lens.json',
                  'type': 'lens',
-                 'focal_length': -1000e-3,
-                 'width': 0}
+                 'focal_length': 1.1,
+                 'width': 2e-3}
 
     qwp = {'name': 'Quarterwave Plate',
            'filename': 'config_qwp.json',
            'type': 'qwp',
            'angle': 2*np.pi/7,
-           'width': 0}
+           'width': 2e-3}
 
     pbc = {'name': 'Polarizing Beam Cube',
            'filename': 'config_pbc.json',
            'type': 'pbc',
            'arm': 'T',
-           'width': 0}
+           'width': 0.25,
+           'GVD': 214.83e-27}
 
     hwp = {'name': 'Halfwave Plate',
            'filename': 'config_hwp.json',
            'type': 'hwp',
            'angle': np.pi/7,
-           'width': 0}
+           'width': 2e-3}
 
     beam_shape_lens_1 = {'name': 'Beam Shape lens 1',
                          'filename': 'config_shape_lens_1.json',
                          'type': 'lens',
                          'focal_length': 100e-3,
-                         'width': 0}
+                         'width': 2e-3}
 
 
     configs_to_make = (coll_lens, beam_shape_lens_1, qwp, pbc, hwp)
