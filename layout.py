2410553071cd730234112fe2b1576608570b7ac4
James Maldaner <maldaner@ualberta.ca>
2026-05-12 16:07:09 -0600

    Updated the layout file

--- layout.py
+++ layout.py
@@ -7,26 +7,27 @@ Verify calculatoins while implementing test cases.
 Add optical elements to plots
 
 """
-
+"""
+This file 
+"""
 
 # Standard library imports
 import json
 from dataclasses import dataclass
 
-from loguru import logger
-
 # Open source library imports
+from loguru import logger
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Custom library imports
-from make_config import initiate_data
 from gaussian_beam_slice import gaussian_beam
 from optical_element_objects import Lens, QWP, HWP, PBC, Space
 
 
 def gauss_envelope(w_0, z_from_w_0, wavelength_center, index = 1):
+    """Returns the beam size at a particulat location. """
     z_R = np.pi * w_0 ** 2 * index / wavelength_center
     return w_0 * np.sqrt(1 + (z_from_w_0 / z_R)**2)
 
@@ -34,35 +35,47 @@ def gauss_envelope(w_0, z_from_w_0, wavelength_center, index = 1):
 class optical_layout:
     """This class runes through a particular optical layout and creates a database for future analysis."""
     init_beam: gaussian_beam  # This is the beam at the point it enters the optical setup
-    element_set: tuple  # This contains two types of elements, either an optical component or a number indicating a distance
+    element_set: tuple  # This contains a list of optical component and the spaces between them
+    step: float = 0.005
     def __post_init__(self):
-        self.df = self.init_df()
-        self.row_calcs()
-        self.make_flat_df()
+        self.df = self.init_df()  # Initiate the dataframe
+        self.row_calcs()  # Do the full calculation for every row
+        self.make_flat_df()  # 
+        
     
     def init_df(self):
-        """This method initiates the dataframe from the element set. """
+        """This method initiates the dataframe from the element set and finds the dz and z. """
         logger.info('Begin intiating dataframe.')
+
+        # Find the width of all the elements
         dz = []
         for index, element in enumerate(self.element_set):
             dz.append(element.width)
+
+        logger.debug(f'{dz=}')
+
+        # Find the z location of each element
         z = [0]
-        logger.info(f'{dz=}')
         for i, new_z in enumerate(dz):
             z.append(z[i] + new_z)
         z.pop()
-        logger.info(f'{z=}')
+
+        logger.debug(f'{z=}')
+        
         df = pd.DataFrame({'z': z, 'element': self.element_set})  
         logger.info('Finished initiating dataframe.')
         return df
 
     def row_calcs(self):
         """Calculate all the rows."""
-        first_beam = self.init_beam
         logger.info('Begin row calculations')
+
+        first_beam = self.init_beam
         self.df.at[0, 'init_beam_profile'] = first_beam
         self.df['final_pol_vec'] = None
         self.df['init_pol_vec'] = None
+
+        # Iterate through the beam propegation path
         for index, row in self.df.iterrows():
             elem = row['element']
             old_beam = self.df.at[index, 'init_beam_profile']
@@ -91,25 +104,27 @@ class optical_layout:
             self.df.at[index, 'width'] = elem.width
             self.df.at[index, 'max_intensity'] = new_beam.I_max
         logger.info('Finished row calculations')
-        logger.info(f'Columns {self.df.columns}')
 
     def make_flat_df(self):
-        step = 0.005
+        """This method makes a df that can be saved to json and re-loaded without the need for the optical elements dataclass."""
+
         logger.info('Creating flat dataframe.')
+
+        step = self.step
         z_lst = np.array(self.df.z)
         dz_lst = np.array(self.df.width)
         element_lst = self.df.element
         beam_start_lst = self.df.init_beam_profile
         index_lst = self.df.index
 
-        # FIRST THING
+        # Initiate the values to be calculated
         z_flat = []
         w_z_flat = []
         z_from_w_0_flat = []
         tau_fwhm = []
         max_intensity = []
 
-
+        # Iterate through the elements
         for index, dz, z, element in zip(index_lst, dz_lst, z_lst, element_lst):
             series = self.df.iloc[index]
             logger.debug(f'{series=}')
@@ -117,6 +132,8 @@ class optical_layout:
             z_from_w_0_start = series['z_from_w_0_start']
 
             lambda_0 = beam_start_lst[index].wavelength_center
+
+            # Analyse across the length of the object.
             if element.width > 0:
                 z_calcs = np.arange(0, dz, step)
                 if np.max(np.shape(z_calcs)) < 1:
@@ -127,24 +144,19 @@ class optical_layout:
                 w_z_calc = np.zeros(np.shape(z_calcs))
                 for i, z_rel in enumerate(z_calcs):
                     w_z_calc[i] = gauss_envelope(w_0, z_from_w_0_start+z_rel, lambda_0)
-                logger.info(f'{w_z_calc[i]=:.3e}, {z_rel=:.3e}, {z_from_w_0_start=:.3e}, {z + z_calcs[i] =}, {str(element).split('(')[0]}')
+                logger.debug(f'{w_z_calc[i]=:.3e}, {z_rel=:.3e}, {z_from_w_0_start=:.3e}, {z + z_calcs[i] =}, {str(element).split('(')[0]}')
 
-                # SECOND THING
                 w_z_flat.extend(w_z_calc)
                 z_flat.extend(z + z_calcs)
                 tau_fwhm.extend(np.ones(np.shape(z_calcs)) * self.df.at[index, 'tau_fwhm_init'])
                 max_intensity.extend(np.ones(np.shape(z_calcs)) * self.df.at[index, 'max_intensity'])
             else:
-
-                # THIRD THING
+                # Do this if the element is essentially width-less.
                 z_flat.append(z)
                 w_z_flat.append(self.df.at[index, 'w_z_start'])
                 tau_fwhm.extend(np.ones(np.shape(z_calcs)) * self.df.at[index, 'tau_fwhm_init'])
                 max_intensity.extend(np.ones(np.shape(z_calcs)) * self.df.at[index, 'max_intensity'])
 
-        plt.plot(z_flat, w_z_flat, '.')
-        plt.savefig('test.png')
-                            # FOURTH THING
         flat_df = pd.DataFrame({'z': z_flat, 'w_z': w_z_flat, 'tau_fwhm': tau_fwhm, 'max_intensity': max_intensity})
         flat_df.to_csv('test_df.csv')
         with open('element_set.json', 'w') as f:
@@ -152,7 +164,6 @@ class optical_layout:
                 
 
 def sim_setup():
-    initiate_data()
     folder = '../config/'
     space_1 = 1.2  # Space before collimating lens
     space_2 = 0.1  # Space between coll_lens and the hwp
